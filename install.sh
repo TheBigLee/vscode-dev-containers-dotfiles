884d2337a75f714afb614dac0b7a64e123381612
Nicolas Bigler <nicolas.bigler@vshn.ch>
2025-05-19 16:52:00 +0200

    Adjust install script
    Signed-off-by: Nicolas Bigler <nicolas.bigler@vshn.ch>

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
 export PATH="$HOME/.local/bin:$PATH"
 
+mkdir -p ${HOME}/.local/bin
+
 echo "Install chezmoi"
-sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply git@github.com:TheBigLee/dot-chezmoi.git
+sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin init --apply git@github.com:TheBigLee/dot-chezmoi.git
+
