96e9636796a77ac99e480edf4790016c6badd114
Nicolas Bigler <nicolas.bigler@vshn.ch>
2025-05-19 10:27:54 +0200

    Adjust chezmoi install url
    Signed-off-by: Nicolas Bigler <nicolas.bigler@vshn.ch>

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 export PATH="$HOME/.local/bin:$PATH"
 
 echo "Install chezmoi"
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:TheBigLee/dot-chezmoi.git
+sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply git@github.com:TheBigLee/dot-chezmoi.git
