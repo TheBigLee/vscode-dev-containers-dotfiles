a8b84f9a4b08903fcf95ba18de9fab0434697531
Bigli <9610820+TheBigLee@users.noreply.github.com>
2025-06-06 10:16:50 +0200

    Remove exa.zip aftert install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ mkdir -p ${HOME}/.local/bin
 
 echo "Install exa"
 export EXAVERSION="0.10.1"
-curl -L -o exa.zip https://github.com/ogham/exa/releases/download/v${EXAVERSION}/exa-linux-x86_64-v${EXAVERSION}.zip && unzip exa.zip -d exa && install -c -m 0755 exa/bin/exa ${HOME}/.local/bin && rm -rf exa
+curl -L -o exa.zip https://github.com/ogham/exa/releases/download/v${EXAVERSION}/exa-linux-x86_64-v${EXAVERSION}.zip && unzip exa.zip -d exa && install -c -m 0755 exa/bin/exa ${HOME}/.local/bin && rm -rf exa && rm exa.zip
 
 echo "Install chezmoi"
 sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin init --apply git@github.com:TheBigLee/dot-chezmoi.git
